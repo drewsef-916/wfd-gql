cf52b86269c91c3eedbba360ada80cb6d5696861
Drew Moore <drewmoore@ip-172-27-222-235.us-east-2.compute.internal>
2019-09-04 08:25:53 -0700

    main js changes

--- src/main.js
+++ src/main.js
@@ -2,17 +2,23 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
-
-const routes = [
-  { path: '/recipes/:id' },
-  { path: '/newRecipe' },
-  { path: '*'}
-]
+import recipe from './components/recipe'
+import newRecipe from './components/newRecipe'
 
 Vue.use(VueRouter)
 
-new Vue({
-  render: h => h(App),
+Vue.config.productionTip = false
+
+const routes = [
+  { path: '/recipe/:id', component: recipe },
+  { path: '/new-recipe', component: newRecipe },
+  { path: '*'}
+]
+
+const router = new VueRouter({
   routes
+})
+
+new Vue({
+  router
 }).$mount('#app')
